b8c8276daff610ab5712f26aef623893c89bcd4c
José Vinícius <Jose.Pontes@brisabr.com.br>
2024-04-21 23:48:38 -0300

    Atualizacao footer

--- src/elements/components/footer/index.tsx
+++ src/elements/components/footer/index.tsx
@@ -6,12 +6,16 @@ import Footer4 from "../../fragments/footer4";
 
 export default function Footer() {
     return(
-        <div>
+        <div id="final-footer">
             <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)' }} id="container">
                 <Footer1/>
                 <Footer2/>
                 <Footer3/>
                 <Footer4/>
+            </div>
+            <div id="line"></div>
+            <div id="rights">
+                <div>2023 furino. All rights reverved</div>
             </div>
         </div>
     );
