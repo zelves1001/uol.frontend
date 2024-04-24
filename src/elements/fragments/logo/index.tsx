7990735f7b3da167ecaf7dd732b755d41aeafbcd
José Vinícius Alves Pontes <66697671+zelves1001@users.noreply.github.com>
2024-04-24 13:51:03 -0300

    Update index.tsx

--- src/elements/fragments/logo/index.tsx
+++ src/elements/fragments/logo/index.tsx
@@ -2,9 +2,14 @@ import "./style.css";
 import LogoPng from './assets/logo.svg';
 
 export default function Logo() {
+
+    const handleClick = () => {
+        window.location.href = '/';
+    };
+
     return(
         <div>
-            <button id='logo-container'>
+            <button onClick={handleClick} id='logo-container'>
                 <img src={LogoPng} alt="" />
                 <span>Furniro</span>
             </button>
