42dd97c8dae488c72281c43974d442e7e45ce1e7
José Vinícius Alves Pontes <66697671+zelves1001@users.noreply.github.com>
2024-04-24 12:06:45 -0300

    Add files via upload

---
+++ src/elements/components/pageGuider/index.tsx
@@ -0,0 +1,21 @@
+import "./style.css";
+import Arrow from './assets/arrow.svg';
+
+export default function PageGuider(_props: any) {
+    return(
+        <div>
+            <div id="page-guider">
+                <div id="guider-links">
+                    <a id="afastar" href="">Home</a>
+                    <img src={Arrow} alt="" />
+                    <a href="">Shop</a>
+                    <img src={Arrow} alt="" />
+                </div>
+                <div id="guider-product">
+                    <div id="vertical-line"></div>
+                    <span>{_props.name}</span>
+                </div>
+            </div>
+        </div>
+    );
+}
