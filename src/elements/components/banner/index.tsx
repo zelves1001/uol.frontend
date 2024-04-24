d8e01324b610d4ccc3906228adcb0851f55f2070
José Vinícius Alves Pontes <66697671+zelves1001@users.noreply.github.com>
2024-04-24 11:25:12 -0300

    Add files via upload

---
+++ src/elements/components/banner/index.tsx
@@ -0,0 +1,21 @@
+import "./style.css";
+import Foto1 from './assets/foto1.png';
+import Arrow from './assets/arrow.svg';
+
+export default function Banner() {
+    return(
+        <div>
+            <div id="banner">
+                <img src={Foto1} alt="" />
+                <div id="banner-text">
+                    <h1>Shop</h1>
+                    <div id="banner-buttons">
+                        <a href="">Home</a>
+                        <img src={Arrow} alt="" />
+                        <span>Shop</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
